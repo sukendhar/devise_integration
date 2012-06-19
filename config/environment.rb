7db4428b2b2fd90cde6b9bade3d925901e5be998
sukendhar <sukendhar555@gmail.com>
2012-06-20 00:46:52 +0530

    device added

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActiveSupport::Deprecation.silenced = true 
 # Initialize the rails application
 Sampleapp::Application.initialize!
